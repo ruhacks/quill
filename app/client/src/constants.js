1301d274b5528bac5bd50e98687e6990d6d85b14
dstingaciu <stingaciu.sebastian@gmail.com>
2019-04-05 19:07:48 -0400

    Fixed several issues -Fixed confirmation part of portal and personalized more for Ryerson -Fixed HTML timezone

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -15,7 +15,7 @@ angular.module('reg')
         ADMITTED_AND_CAN_CONFIRM_TITLE: 'You must confirm by [CONFIRM_DEADLINE].',
         ADMITTED_AND_CANNOT_CONFIRM_TITLE: 'Your confirmation deadline of [CONFIRM_DEADLINE] has passed.',
         ADMITTED_AND_CANNOT_CONFIRM: 'Although you were accepted, you did not complete your confirmation in time.\nUnfortunately, this means that you will not be able to attend the event, as we must begin to accept other applicants on the waitlist.\nWe hope to see you again next year!',
-        CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until [CONFIRM_DEADLINE]',
+        CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until [CONFIRM_DEADLINE] (EDT)',
         DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to RU HACKS 2019! :(\nMaybe next year! We hope you see you again soon.',
     })
     .constant('TEAM',{
